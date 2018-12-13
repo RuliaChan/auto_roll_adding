a00a34980d767f23b9b6ca9543e0712db140d4ff
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2018-12-13 17:42:03 +0900

    readmeを追加、完成

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import yaml
 
 # mainクラス
 class Main(discord.Client):
+    # 実行待機状態のLog
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
     
@@ -17,14 +18,34 @@ class Main(discord.Client):
         self.female = data['female_profile']
         self.LGBTQ = data['LGBTQ_profile']
     
+    # メッセージを監視するメソッド
     async def on_message(self, message):
-        if (message.channel.id == self.male):
-            print(message.author.name + 'に男性役職を付与')
-            male = discord.Role('@男性')
-            await message.author.add_roles(male)
 
+        # 男性プロフィールの場合
+        if (message.channel.id == self.male):
+
+            # log
+            print(message.author.name + 'に男性役職を付与')
+
+            male_role = discord.utils.find(lambda role: role.name == '男性', message.guild.roles)
+            await message.author.add_roles(male_role)
+
+        # 女性プロフィールの場合
         if (message.channel.id == self.female):
+            #log
             print(message.author.name + 'に女性役職を付与')
+
+            female_role = discord.utils.find(lambda role: role.name == '女性', message.guild.roles)
+            await message.author.add_roles(female_role)
+        
+        # LGBTQプロフィールの場合
+        if (message.channel.id == self.LGBTQ):
+
+            # log
+            print(message.author.name + 'にLGBTQ役職を付与')
+
+            lgbtq_role = discord.utils.find(lambda role: role.name == 'LGBTQ', message.guild.roles)
+            await message.author.add_roles(lgbtq_role)
 
     # コンストラクタ
     def __init__(self):
